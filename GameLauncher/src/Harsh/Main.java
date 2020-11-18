db7d2fe93991d8a6e1f4b163652930779552a321
harshLAD <harshk.lad@gmail.com>
2020-11-18 13:25:39 -0500

    Created sample OOP porject

---
+++ GameLauncher/src/Harsh/Main.java
@@ -0,0 +1,9 @@
+package Harsh;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    GuessGame game = new GuessGame();
+	    game.startGame();
+    }
+}
--- GamesLauncher_test/src/coe318/lab3/Main.java
+++
@@ -1,8 +0,0 @@
-package coe318.lab3;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
